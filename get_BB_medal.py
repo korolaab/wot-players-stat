775ab06d5a83f70176a55b848cbc88e1b3af3b93
korolaab <gusarov.alex3@ya.ru>
2020-02-17 20:22:10 +0100

    very bad code to fixing problems

--- get_BB_medal.py
+++ get_BB_medal.py
@@ -9,10 +9,10 @@ Yusha_medal="medalBobYusha"
 Amway_medal="medalBobAmway921"
 medals=[Korben_medal,Lebwa_medal,Yusha_medal,Amway_medal]
 
-with open(folder+"app_id","r") as f:
+with open("app_id.txt","r") as f:
     app_id = f.read()
-with open(folder+"clickhouse_host.json") as f:
-    db = json.load(f.read())  
+with open("clickhouse_host.json") as f:
+    host = json.loads(f.read())  
 
 
 def get_account_id(nicknames):
@@ -22,26 +22,56 @@ def get_account_id(nicknames):
             return raw_data["data"]
         else:
             return []
-def get_bb_medal(ids):
-    json_data=requests.get("https://api.worldoftanks.ru/wot/account/achievements/?application_id={}&fields=achievements%2C&account_id={}".format(app_id,','.join(str(e) for e in [*ids]))).text
+def get_bb_medal(ids_list):
+    json_data=requests.get("https://api.worldoftanks.ru/wot/account/achievements/?application_id={}&fields=achievements%2C&account_id={}".format(app_id,','.join(str(e)
+    for e in ids_list))).text
     raw_data=json.loads(json_data)
+    ids={}
     if(raw_data["status"] == "ok"):
-        for i in [*ids]:
-            for medal in medals:                
-                if(medal in raw_data["data"][str(i)]["achievements"]):
-                    ids[i]["medal"]=medal                    
-                    break
+        for i in ids_list:
+            if(raw_data["data"][str(i)] != None):
+                ids[str(i)]={"medal":"No_data"}
+                for medal in medals:                
+                    if(medal in raw_data["data"][str(i)]["achievements"]):
+                        ids[str(i)]["medal"]=medal                    
+                        break
     return ids
                 
 def get_stat(ids):
     json_data=requests.get("https://api.worldoftanks.ru/wot/account/info/?application_id={}&account_id={}&fields=statistics.all".format(app_id,','.join(str(e) for e in [*ids]))).text
     raw_data=json.loads(json_data)
+    accounts = []
     if(raw_data["status"]=="ok"):
         for i in [*ids]:
-            ids[i]["wins"]=raw_data["data"][str(i)]["statistics"]["all"]["wins"]
-            ids[i]["losses"]=raw_data["data"][str(i)]["statistics"]["all"]["losses"]
-            ids[i]["draws"]=raw_data["data"][str(i)]["statistics"]["all"]["draws"]
-            ids[i]["battle_avg_xp"]=raw_data["data"][str(i)]["statistics"]["all"]["battle_avg_xp"]
-            ids[i]["damage_dealt"]=raw_data["data"][str(i)]["statistics"]["all"]["damage_dealt"]
-    return ids
+            if("wins" in raw_data["data"][i]["statistics"]["all"]):
+                accounts.append({'id':int(i),
+                                "bb_team":ids[i]['medal'],
+                                'wins':raw_data["data"][i]["statistics"]["all"]["wins"],
+                                'losses':raw_data["data"][i]["statistics"]["all"]["losses"],
+                                'draws':raw_data["data"][i]["statistics"]["all"]["draws"],
+                                'avg_xp':raw_data["data"][i]["statistics"]["all"]["battle_avg_xp"],
+                                'total_damage':raw_data["data"][i]["statistics"]["all"]["damage_dealt"]})
+            else: #if i is press account or something get wrong
+                accounts.append({'id':int(i),
+                                'bb_team':"No_data",
+                                'wins':0,
+                                'losses':0,
+                                'draws':0,
+                                'avg_xp':0,
+                                'total_damage':0})
 
+    return accounts
+
+def write_to_db(accounts):
+        log = client.execute("INSERT INTO wot.player_stat VALUES",accounts,types_check=True)
+        return log
+client = clickhouse_driver.Client(host=host["ip"])
+
+with open("last_account_id.txt","r") as f:
+    last_account_id = int(f.read())# read last accoutn id
+ids_list=list(range(last_account_id,last_account_id+100))
+acc_bb_medals = get_bb_medal(ids_list)
+accounts = get_stat(acc_bb_medals)
+write_to_db(accounts)
+with open("last_account_id.txt","w") as f:
+    f.write(str(last_account_id+100))
